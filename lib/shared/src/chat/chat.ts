dab46530daf3b9cdb9509f26f1260138d083ea4d
iohub <rickyang.pro@gmail.com>
2024-01-03 22:24:46 +0800

    VSCode: add llama.cpp maxTokens config

--- lib/shared/src/chat/chat.ts
+++ lib/shared/src/chat/chat.ts
@@ -1,29 +1,34 @@
 import { ANSWER_TOKENS } from '../prompt/constants'
 import { Message } from '../sourcegraph-api'
+import * as vscode from 'vscode'
 import type { SourcegraphCompletionsClient } from '../sourcegraph-api/completions/client'
 import type { CompletionCallbacks, CompletionParameters } from '../sourcegraph-api/completions/types'
 
 type ChatParameters = Omit<CompletionParameters, 'messages'>
 
-const DEFAULT_CHAT_COMPLETION_PARAMETERS: ChatParameters = {
-    temperature: 0.2,
-    maxTokensToSample: ANSWER_TOKENS,
-    top_k: 20,
-    top_p: 0.85,
-    stream: true,
-    n_predict: 800,
-}
-
 export class ChatClient {
-    constructor(private completions: SourcegraphCompletionsClient) {}
+    private maxTokens: number
+
+    constructor(private completions: SourcegraphCompletionsClient) {
+        let config = vscode.workspace.getConfiguration()
+        this.maxTokens = config.get<number>('cody.llama.maxTokens', 100)
+    }
 
     public chat(messages: Message[], cb: CompletionCallbacks, params?: Partial<ChatParameters>): () => void {
         const isLastMessageFromHuman = messages.length > 0 && messages.at(-1)!.speaker === 'human'
         const augmentedMessages = isLastMessageFromHuman ? messages.concat([{ speaker: 'assistant' }]) : messages
+        const defaults: ChatParameters = {
+            temperature: 0.6,
+            maxTokensToSample: ANSWER_TOKENS,
+            top_k: 20,
+            top_p: 0.85,
+            stream: true,
+            n_predict: this.maxTokens
+        }
 
         return this.completions.stream(
             {
-                ...DEFAULT_CHAT_COMPLETION_PARAMETERS,
+                ...defaults,
                 ...params,
                 messages: augmentedMessages,
             },
